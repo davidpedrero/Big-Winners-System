6f5568e7e7627d34a91b4c95faabb79074085246
David Pedrero <pedrero888@gmail.com>
2023-02-03 13:34:06 -0500

    Building HomeScreen/Dashboard out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,9 +9,9 @@ function App() {
   return (
     <>
       {/* <LoginScreen />  */}
-      <ProfileScreen />
+      {/* <ProfileScreen /> */}
       {/* <SearchScreen /> */}
-      {/* <HomeScreen /> */}
+      <HomeScreen />
     </>
   );
 }
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -5,13 +5,21 @@ import './Dashboard.css';
 export const Dashboard = () => {
     return (
         <div className='dashboard-container'>
-        <h1>Home</h1>
         <div className='dashboard-content'>
-        </div>
-        <div className='dashboard-grid'>
-            <input className='search-box' type="text" placeholder='Full Name' />
-            <input className='search-box' type="text" placeholder='Order ID'/>
-            <input className='search-box' type="text" placeholder='Customer ID' />
+            <div className='state-tabs'>
+                <button class="tablinks" onclick="">New Jersey</button>
+                <button class="tablinks" onclick="">New York</button>
+                <button class="tablinks" onclick="">Texas</button>
+                <button class="tablinks" onclick="">Arkansas</button>
+                <button class="tablinks" onclick="">Minnesota</button>
+            </div>
+            <div className='dashboard-grid'>
+                <span>Full Name</span>
+                <span>Order ID</span>
+                <span>Win Draw Date</span>
+                <span>Ready for Mailout</span>
+                <span>Delivery Status</span>
+            </div>
         </div>
     </div>
     )
